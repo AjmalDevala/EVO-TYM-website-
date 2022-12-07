2c97c25d8f492896948221c5c88a61e0a2d38d35
AjmalDevala <ajmaldevala@gmail.com>
2022-12-07 10:21:40 +0530

    config changed

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,9 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost:27017/EVOTYM');
+// mongoose.connect('mongodb://localhost:27017/EVOTYM');
+ mongoose.connect('mongodb+srv://ajmal:VOFCNb2U89qYgltH@cluster0.a5frclp.mongodb.net/EVOTYM?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true');
+
 
 const db =  mongoose.connection
 
